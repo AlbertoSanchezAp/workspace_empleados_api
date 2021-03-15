47245e4a951c8de52c4eff59186b7af6495d8407
AlbertoSanchezAp <jose.nava@coppel.com>
2021-03-15 00:45:32 -0700

    se actualiza codigo para ejecucion de spl y se agrega nuevo servicio rest para actualizacion de datos
    Signed-off-by: AlbertoSanchezAp <jose.nava@coppel.com>

---
+++ empleados_apiJava/desarrollo/api/empleados/error_respuesta.java
@@ -0,0 +1,63 @@
+package desarrollo.api.empleados;
+
+import java.util.Properties;
+import com.ibm.broker.javacompute.MbJavaComputeNode;
+import com.ibm.broker.plugin.MbElement;
+import com.ibm.broker.plugin.MbException;
+import com.ibm.broker.plugin.MbJSON;
+import com.ibm.broker.plugin.MbMessage;
+import com.ibm.broker.plugin.MbMessageAssembly;
+import com.ibm.broker.plugin.MbOutputTerminal;
+import com.ibm.broker.plugin.MbUserException;
+
+public class error_respuesta extends MbJavaComputeNode {
+
+	public void evaluate(MbMessageAssembly inAssembly) throws MbException {
+		MbOutputTerminal out = getOutputTerminal("out");
+
+		MbMessage inMessage = inAssembly.getMessage();
+		MbMessageAssembly outAssembly = null;
+		try {
+
+			MbMessage outMessage = new MbMessage();
+			outAssembly = new MbMessageAssembly(inAssembly, outMessage);
+			MbElement outRoot = outMessage.getRootElement();
+
+			MbElement outHTTPRequest = outRoot.createElementAsLastChild("HTTPRequestHeader");
+			outHTTPRequest.createElementAsLastChild(MbElement.TYPE_NAME_VALUE, "Content-Type", "application/json; charset=utf-8"); //charset=utf-8 || charset=ISO-8859-1
+
+			MbElement outJsonRoot = outRoot.createElementAsLastChild(MbJSON.PARSER_NAME);
+			MbElement outJsonData = outJsonRoot.createElementAsLastChild(MbElement.TYPE_NAME, MbJSON.DATA_ELEMENT_NAME, null);
+
+			Properties props = new Properties();
+			MbElement entrada = inMessage.getRootElement().getLastChild().getFirstElementByPath("/JSON/Data").getFirstChild();
+			
+			
+			while(entrada != null ) {			
+				props.setProperty( entrada.getName(), entrada.getValueAsString() );	
+				entrada = entrada.getNextSibling();
+			} 
+			    	 outJsonData.createElementAsLastChild(MbElement.TYPE_NAME_VALUE,"codigoRespuesta","11111");
+			     	 outJsonData.createElementAsLastChild(MbElement.TYPE_NAME_VALUE,"mensaje","Ha ocurrido un error al realizar la operacion");
+	               
+			   
+			// End of user code
+			// ----------------------------------------------------------
+		} catch (MbException e) {
+			// Re-throw to allow Broker handling of MbException
+			throw e;
+		} catch (RuntimeException e) {
+			// Re-throw to allow Broker handling of RuntimeException
+			throw e;
+		} catch (Exception e) {
+			// Consider replacing Exception with type(s) thrown by user code
+			// Example handling ensures all exceptions are re-thrown to be handled in the flow
+			throw new MbUserException(this, "evaluate()", "", "", e.toString(),
+					null);
+		}
+		// The following should only be changed
+		// if not propagating message to the 'out' terminal
+		out.propagate(outAssembly);
+
+	}
+}
